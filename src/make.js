ad1ef54f7683aa238d51758ae1dc60c3538b0be7
Eugene Rebedailo <mokonamodokidesu@gmail.com>
2018-04-11 12:13:49 +0300

    Refactored make.js a bit, added comments, fixed package.json

--- src/make.js
+++ src/make.js
@@ -1,11 +1,12 @@
 module.exports = function make(...args) {
     const argCount = args.length;
-    if (args[argCount - 1] instanceof Function) {
-        const reduceFn = args[argCount - 1];
+    const lastArg = args[argCount - 1];
+    if (lastArg instanceof Function) {
         return args
             .slice(0, argCount - 1)
-            .reduce(reduceFn);
+            .reduce(lastArg);
     } else {
+        // Guess this is kind of like recursion since we are binding to the "make" fn.
         return make.bind(this, ...args);
     }
 }
